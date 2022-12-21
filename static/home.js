cf39b844ec122d1e150f3e38ff458280854bf142
ber-nardTheo1 <70895887+ber-nardTheo1@users.noreply.github.com>
2022-12-21 17:33:07 +0900

    mywebsite

--- static/home.js
+++
@@ -1,7 +0,0 @@
-const LeftNavLink = document.getElementsById('left-nav-bar')
-const ToggleButton = document.getElementById('toggle-button')
-
-ToggleButton.addEventListener('click', () =>{
-    LeftNavLink.ClassList.toggle('active')
-
-})
